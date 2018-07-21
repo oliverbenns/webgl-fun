6978924cd375454f0a6a02ffad1632f6d5b405e2
Oliver Benns <oliverbenns@gmail.com>
2018-07-21 18:25:14 +0200

    Add square/rects scene

--- src/scenes/square.ts
+++ src/scenes/square.ts
@@ -6,22 +6,39 @@ import Vector from 'core/objects/Vector';
 
 const scene = new Scene();
 
-const one = new Entity({
-  colors: [
-    Color.fromHex('#3498db'),
-    Color.fromHex('#a569bd'),
-    Color.fromHex('#3498db'),
-  ],
-  vertices: [
-    new Vector(200, 200),
-    new Vector(300, 200),
-    new Vector(200, 300),
-  ],
-});
+class Rectangle extends Entity {
+  constructor(width: number, height: number) {
+    super({
+      colors: [
+        Color.fromHex('#3498db'), // a
+        Color.fromHex('#a569bd'), // b
+        Color.fromHex('#808000'), // c
+        Color.fromHex('#808000'), // c
+        Color.fromHex('#a569bd'), // b
+        Color.fromHex('#ff9e00'), // d
+      ],
+      vertices: [
+        new Vector(0, 0),
+        new Vector(width, 0),
+        new Vector(0, height),
+        new Vector(0, height),
+        new Vector(width, 0),
+        new Vector(width, height),
+      ],
+    })
+  }
+}
 
-one.velocity.x = 0.25;
-one.velocity.y = 0.25;
-
+const one = new Rectangle(100, 100);
+one.velocity.y = 10;
+one.position.x = 150;
+one.position.y = 150;
 scene.add(one);
 
-export default scene
+const two = new Rectangle(100, 100);
+two.position.x = 400;
+two.position.y = 300;
+two.velocity.y = -10;
+scene.add(two);
+
+export default scene;
