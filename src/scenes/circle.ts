12deaa3d0681cf89397aa0d8ebf9d4744b231feb
Oliver Benns <oliverbenns@gmail.com>
2018-07-22 12:50:44 +0200

    Add basic circle geometry

--- src/scenes/circle.ts
+++ src/scenes/circle.ts
@@ -3,34 +3,48 @@ import Color from 'core/objects/Color';
 import Entity from 'core/objects/Entity';
 import Scene from 'core/objects/Scene';
 import Vector from 'core/objects/Vector';
+import _angle from 'core/lib/angle';
 
 const scene = new Scene();
 
 class Circle extends Entity {
-  constructor(radius: number) {
-    super({
-      colors: [
-        Color.fromHex('#3498db'), // a
-        Color.fromHex('#a569bd'), // b
-        Color.fromHex('#808000'), // c
-        Color.fromHex('#808000'), // c
-        Color.fromHex('#a569bd'), // b
-        Color.fromHex('#ff9e00'), // d
-      ],
-      vertices: [
-        new Vector(0, 0),
-        new Vector(radius, 0),
-        new Vector(0, radius),
-        new Vector(0, radius),
-        new Vector(radius, 0),
-        new Vector(radius, radius),
-      ],
-    })
+  constructor(radius: number, polyCount: number) {
+    const vertices: Vector[] = [];
+    const colors: Color[] = [];
+
+    const rotateVector = (v: Vector, angle: number) => new Vector(
+      v.x * Math.cos(angle) - v.y * Math.sin(angle),
+      v.x * Math.sin(angle) + v.y * Math.cos(angle),
+    );
+
+    const baseTriangle = [
+      new Vector(0, 0),
+      new Vector(0, radius),
+      new Vector(radius, 0),
+    ];
+
+    const iterationAngle = 360 / polyCount
+
+    for (let i = 0; i < polyCount; i++) {
+      const angle = _angle.degreesToRadians(iterationAngle * i);
+
+      vertices.push(rotateVector(baseTriangle[0], angle))
+      vertices.push(rotateVector(baseTriangle[1], angle))
+      vertices.push(rotateVector(baseTriangle[2], angle))
+
+      colors.push(Color.fromHex('#dddddd'));
+      colors.push(Color.fromHex('#3498db'));
+      colors.push(Color.fromHex('#3498db'));
+    }
+
+    super({ colors, vertices });
   }
 }
 
-const one = new Circle(100);
-
+const one = new Circle(100, 18);
+one.position.x = 300
+one.position.y = 300
 scene.add(one);
+
 
 export default scene
