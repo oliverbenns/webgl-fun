8306c41be9532259beb5af044dadb2172a13f9c8
Oliver Benns <oliverbenns@gmail.com>
2018-09-05 10:27:21 +0100

    Add animation

--- src/scenes/square.ts
+++ src/scenes/square.ts
@@ -36,64 +36,55 @@ class Rectangle extends Entity {
 
     this.startTime = Date.now();
   }
+
+  // @TODO: Need some sort of setup method to determine start point.
+
+  update(deltaTime: number) {
+    const newTime = Date.now();
+    const duration = 1.5;
+
+    const timeSinceLoopStart = newTime - this.startTime
+    const loopProgress = timeSinceLoopStart / duration // ms
+    const progressInPercent = loopProgress / 1000
+
+    if (progressInPercent < 0.25) {
+      const pathProgressInPercent = progressInPercent / 0.25;
+      const targetPosition = lerp(100, 400, pathProgressInPercent);
+
+      this.position.x = clamp(targetPosition, 100, 400);
+    }
+
+    else if (progressInPercent >= 0.25 && progressInPercent < 0.5) {
+      const pathProgressInPercent = (progressInPercent - 0.25) * 4;
+      const targetPosition = lerp(300, 100, pathProgressInPercent);
+      this.position.y = clamp(targetPosition, 300, 100);
+    }
+
+    else if (progressInPercent >= 0.5 && progressInPercent <= 0.75) {
+      const pathProgressInPercent = (progressInPercent - 0.5) * 4;
+      const targetPosition = lerp(400, 100, pathProgressInPercent);
+
+      this.position.x = clamp(targetPosition, 400, 100);
+    }
+
+    else if (progressInPercent >= 0.75 && progressInPercent <= 1) {
+      const pathProgressInPercent = (progressInPercent - 0.75) * 4;
+      const targetPosition = lerp(100, 300, pathProgressInPercent);
+
+      this.position.y= clamp(targetPosition, 100, 300);
+    }
+
+    if (progressInPercent >= 1) {
+      this.startTime = Date.now()
+    }
+  }
 }
 
 const one = new Rectangle(100, 100);
 scene.add(one);
-
-one.update = function(deltaTime: number) {
-  const newTime = Date.now();
-  const duration = 2;
-
-  const timeSinceLoopStart = newTime - this.startTime
-  const loopProgress = timeSinceLoopStart / duration // ms
-  const progressInPercent = loopProgress / 1000
-
-  if (progressInPercent < 0.25) {
-    // @TODO: this should be going up on each iteration. Fix the math.
-    const pathProgressInPercent = (0.25 - progressInPercent) * 4;
-    const targetPosition = lerp(100, 400, pathProgressInPercent);
-    console.log('pathProgressInPercent', pathProgressInPercent);
-
-    this.position.x = clamp(targetPosition, 100, 400);
-    return;
-  }
-
-  if (progressInPercent >= 0.25 && progressInPercent <= 0.5) {
-    const pathProgressInPercent = (0.5 - progressInPercent) * 4;
-    const targetPosition = lerp(300, 100, pathProgressInPercent);
-
-    this.position.y = clamp(targetPosition, 300, 100);
-    return;
-  }
-
-  if (progressInPercent >= 0.5 && progressInPercent <= 0.75) {
-    const pathProgressInPercent = (0.75 - progressInPercent) * 4;
-    const targetPosition = lerp(400, 100, pathProgressInPercent);
-
-    this.position.x = clamp(targetPosition, 400, 100);
-    return;
-  }
-
-  if (progressInPercent >= 0.75 && progressInPercent <= 1) {
-    const pathProgressInPercent = (1 - progressInPercent) * 4;
-    const targetPosition = lerp(100, 300, pathProgressInPercent);
-
-    this.position.y= clamp(targetPosition, 100, 300);
-    return;
-  }
-
-  if (progressInPercent >= 1) {
-    this.startTime = Date.now()
-  }
-}
-
-// one.position.y = 300
+one.position.y = 300
 
 const two = new Rectangle(100, 100);
-two.position.x = 200;
-two.position.y = 300;
-two.velocity.y = -10;
 scene.add(two);
 
 export default scene;
