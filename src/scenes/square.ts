e7ab9bd3b5f0dd68cc000eb1a90545c4251ee4b9
Oliver Benns <oliverbenns@gmail.com>
2018-09-08 10:36:27 +0100

    Fix rectangles

--- src/scenes/square.ts
+++ src/scenes/square.ts
@@ -9,14 +9,12 @@ import clamp from 'core/lib/clamp';
 const scene = new Scene();
 
 interface Options {
-  progressInPercent?: number
+  loopOffset?: number
 }
 
 class Rectangle extends Entity {
   private loopDuration: number
-  private progressInPercent: number
-  private timeSinceLoopStart: number
-
+  private loopProgress: number
 
   constructor(width: number, height: number, options: Options = {}) {
     super({
@@ -38,69 +36,63 @@ class Rectangle extends Entity {
       ],
     })
 
-    this.progressInPercent = options.progressInPercent || 0;
-    this.loopDuration = 4;
-    this.timeSinceLoopStart = 0
+    this.loopProgress = options.loopOffset || 0;
+    this.loopDuration = 3;
   }
 
   updateLoopProgress(deltaTime: number) {
-    this.timeSinceLoopStart += deltaTime;
-    // @TODO: do not run first time or somehow make use of initial progressInPercent.
+    const incrementTime = deltaTime / this.loopDuration; // add duration
 
-    const loopProgress = this.timeSinceLoopStart / this.loopDuration;
-    const isFirstLoop = loopProgress < this.progressInPercent;
+    this.loopProgress += incrementTime;
 
-    // The time should already be halfway through. Not at the start.
-
-    if (isFirstLoop) {
-      this.progressInPercent += loopProgress;
-    }
-    else {
-      this.progressInPercent = loopProgress;
+    if (this.loopProgress > 1) {
+      this.loopProgress = 0;
     }
   }
 
   update(deltaTime: number) {
     this.updateLoopProgress(deltaTime);
 
-    if (this.progressInPercent < 0.25) {
-      const pathProgressInPercent = this.progressInPercent / 0.25;
+    if (this.loopProgress < 0.25) {
+      const pathProgressInPercent = this.loopProgress / 0.25;
       const targetPosition = lerp(100, 400, pathProgressInPercent);
 
-      this.position.x = clamp(targetPosition, 100, 400);
+      this.position.x = targetPosition;
+      this.position.y = 300;
     }
 
-    else if (this.progressInPercent >= 0.25 && this.progressInPercent < 0.5) {
-      const pathProgressInPercent = (this.progressInPercent - 0.25) * 4;
+    else if (this.loopProgress >= 0.25 && this.loopProgress < 0.5) {
+      const pathProgressInPercent = (this.loopProgress - 0.25) * 4;
       const targetPosition = lerp(300, 100, pathProgressInPercent);
-      this.position.y = clamp(targetPosition, 300, 100);
+      this.position.y = targetPosition;
+      this.position.x = 400;
     }
 
-    else if (this.progressInPercent >= 0.5 && this.progressInPercent <= 0.75) {
-      const pathProgressInPercent = (this.progressInPercent - 0.5) * 4;
+    else if (this.loopProgress >= 0.5 && this.loopProgress <= 0.75) {
+      const pathProgressInPercent = (this.loopProgress - 0.5) * 4;
       const targetPosition = lerp(400, 100, pathProgressInPercent);
 
-      this.position.x = clamp(targetPosition, 400, 100);
+      this.position.x = targetPosition;
+      this.position.y = 100;
     }
 
-    else if (this.progressInPercent >= 0.75 && this.progressInPercent <= 1) {
-      const pathProgressInPercent = (this.progressInPercent - 0.75) * 4;
+    else if (this.loopProgress >= 0.75 && this.loopProgress <= 1) {
+      const pathProgressInPercent = (this.loopProgress - 0.75) * 4;
       const targetPosition = lerp(100, 300, pathProgressInPercent);
 
-      this.position.y= clamp(targetPosition, 100, 300);
-    }
-
-    if (this.progressInPercent >= 1) {
-      this.progressInPercent = 0;
-      this.timeSinceLoopStart = 0;
+      this.position.y = targetPosition;
+      this.position.x = 100;
     }
   }
 }
 
-// const one = new Rectangle(100, 100);
-// scene.add(one);
+const rectCount = 2;
 
-const two = new Rectangle(100, 100, { progressInPercent: 0.5 });
-scene.add(two);
+for(let i = 0; i < rectCount; i++) {
+  const loopOffset = i / rectCount;
+  const rect = new Rectangle(40, 40, { loopOffset });
+
+  scene.add(rect);
+}
 
 export default scene;
