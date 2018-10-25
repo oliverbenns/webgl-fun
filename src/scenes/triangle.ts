b85d78b440ce7b2503099ca4b2dfdc6353489955
Oliver Benns <oliverbenns@gmail.com>
2018-10-25 23:45:47 +0100

    Tidy up and rotate triangles

--- src/scenes/triangle.ts
+++ src/scenes/triangle.ts
@@ -6,45 +6,39 @@ import Vector from 'core/objects/Vector';
 
 const scene = new Scene();
 
-const one = new Entity({
-  colors: [
-    Color.fromHex('#ff0b00'),
-    Color.fromHex('#00c4ff'),
-    Color.fromHex('#ff0b00'),
-  ],
-  vertices: [
-    new Vector(0, 0),
-    new Vector(100, 0),
-    new Vector(0, 100),
-  ],
-})
+class Triangle extends Entity {
+  constructor() {
+    const colors = [
+      Color.fromHex('#00c4ff'),
+      Color.fromHex('#ff0b00'),
+      Color.fromHex('#ff0b00'),
+    ];
 
-one.position = new Vector(150, 150);
+    const vertices = [
+      new Vector(0, 0),
+      new Vector(100, 0),
+      new Vector(0, 100),
+    ];
 
-one.update = function(deltaTime: number) {
-  this.rotation += Math.PI * deltaTime;
+    super({ colors, vertices })
+  }
+
+  update(deltaTime: number) {
+    this.rotation += Math.PI * deltaTime;
+  }
 }
 
-const two = new Entity({
-  colors: [
-    Color.fromHex('#01cd74'),
-    Color.fromHex('#fff200'),
-    Color.fromHex('#01cd74'),
-  ],
-  vertices: [
-    new Vector(0, 0),
-    new Vector(100, 0),
-    new Vector(100, 100),
-  ],
-})
+const canvasWidth = 640;
+const canvasHeight = 480;
 
-two.position = new Vector(400, 300);
+const one = new Triangle();
+one.position = new Vector(canvasWidth / 2, canvasHeight / 2);
 
-two.update = function(deltaTime: number) {
-  this.rotation += Math.PI * deltaTime;
-}
+const two = new Triangle();
+two.position = new Vector(canvasWidth / 2, canvasHeight / 2);
+two.rotation = Math.PI;
 
-scene.add(two);
 scene.add(one);
+scene.add(two);
 
 export default scene
